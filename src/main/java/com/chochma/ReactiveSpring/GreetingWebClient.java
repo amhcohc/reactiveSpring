1df472a4281d592c25c9b9f10436a9f2a2d7c6bb
Labong <chochma@coupang.com>
2018-01-01 18:36:23 +0900

    Setting Reactive

---
+++ src/main/java/com/chochma/ReactiveSpring/GreetingWebClient.java
@@ -0,0 +1,22 @@
+package com.chochma.ReactiveSpring;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by labong on 2018. 1. 1..
+ */
+public class GreetingWebClient {
+    private WebClient client = WebClient.create("http://localhost:8080");
+
+    private Mono<ClientResponse> result = client.get()
+            .uri("/hello")
+            .accept(MediaType.TEXT_PLAIN)
+            .exchange();
+
+    public String getResult() {
+        return ">> result = " + result.flatMap(res -> res.bodyToMono(String.class)).block();
+    }
+}
